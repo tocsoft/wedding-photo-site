6ebab312874a77896be5474c9c0456492db2356c
Scott <scott@icecreates.com>
2011-06-03 19:35:06 +0100

    inital commit

---
+++ generator/GenerateWeddingHtml/Program.cs
@@ -0,0 +1,265 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using gma.Drawing.ImageInfo;
+using System.Drawing.Imaging;
+
+namespace GenerateWeddingHtml
+{
+    class Program
+    {
+        const string pictureThumbHtml = @"<img src='images/thumbs/{0}/{1}' id='{5}' title='{2}' data-attribution='{3}'  data-attribution-url='{4}' href='images/mains/{0}/{1}'/>";
+        const string albumHtml = @"<li class='album'>
+					<span class='st_link'><span class='st_arrow_down'>{0}</span></span>
+					<div class='st_wrapper st_thumbs_wrapper'>
+						<div class='st_thumbs'>
+                            {1}
+						</div>
+					</div>
+				</li>";
+
+        
+
+        static void Main(string[] args)
+        {
+            string dir = @"..\..\..\..\albums";
+            string thumbs = @"..\..\..\..\Site\images\thumbs";
+            string mains = @"..\..\..\..\Site\images\mains";
+
+            StringBuilder sb = new StringBuilder();
+
+            var albumbs = Directory.GetDirectories(dir);
+            foreach (var a_path in albumbs)
+            {
+                var albumDirName = Path.GetFileName(a_path);
+                var albumName = albumDirName.Split('.').Last().Trim();
+
+                
+                StringBuilder thumbshtml = new StringBuilder();
+
+                foreach (var img_path in Directory.GetFiles(a_path))
+                {
+                    var imageName = Path.GetFileName(img_path);
+                   
+                    Info inf = new Info(img_path);
+                    var artist_details = (inf.Artists ?? new string[]{});
+                    var artist = artist_details.FirstOrDefault() ?? "";
+                    var albumFoldername = albumName.Replace(' ', '_');
+                    var aUrl = artist_details.Select(x => x.Trim()).Where(x => x.StartsWith("http://") || x.StartsWith("https://") || x.StartsWith("mailto:")).FirstOrDefault() ?? "";
+                    var imgId = albumFoldername + "_" + Path.GetFileNameWithoutExtension(img_path);
+
+                    thumbshtml.AppendLine(string.Format(pictureThumbHtml, albumFoldername, imageName, inf.Title ?? imageName, artist, aUrl, imgId));
+                   
+
+                    using (Image img = inf.Image)
+                    {
+
+                        using (var smallImg = Resize(img, new Size(180, 120), ResizeMode.Normal, Color.Black, true))
+                        {
+                            var dirInfo = new DirectoryInfo(thumbs);
+                            var thumbImagePathDir = Path.Combine(dirInfo.FullName, albumFoldername);
+
+
+                            var thumbImagePath = Path.Combine(thumbImagePathDir, Path.GetFileNameWithoutExtension(imageName) + ".jpg");
+
+
+                            if (!Directory.Exists(thumbImagePathDir))
+                                Directory.CreateDirectory(thumbImagePathDir);
+
+
+                            SaveJpeg(thumbImagePath, smallImg, 75);
+
+                        }
+                        using (var largeImage = Resize(img, new Size(600, 800), ResizeMode.Normal, Color.Black, true))
+                        {
+                            var dirInfo = new DirectoryInfo(mains);
+                            var largeImagePathDir = Path.Combine(dirInfo.FullName, albumFoldername);
+
+
+                            var largeImagePath = Path.Combine(largeImagePathDir, Path.GetFileNameWithoutExtension(imageName)+".jpg");
+
+
+                            if (!Directory.Exists(largeImagePathDir))
+                                Directory.CreateDirectory(largeImagePathDir);
+
+                            
+                            SaveJpeg(largeImagePath, largeImage, 90);
+                        }
+                    }
+
+                }
+
+                sb.AppendLine(string.Format(albumHtml, albumName, thumbshtml));
+            }
+
+            var html = File.ReadAllText("index.html").Replace("<!--INSERT ALBUM HTML HERE-->", sb.ToString());
+            File.WriteAllText(@"..\..\..\..\Site\index.html", html);
+        }
+
+        public static void SaveJpeg(string path, Image img, int quality)
+        {
+            if (quality < 0 || quality > 100)
+                throw new ArgumentOutOfRangeException("quality must be between 0 and 100.");
+
+
+            // Encoder parameter for image quality 
+            EncoderParameter qualityParam =
+                new EncoderParameter(System.Drawing.Imaging.Encoder.Quality, quality);
+            // Jpeg image codec 
+            ImageCodecInfo jpegCodec = GetEncoderInfo("image/jpeg");
+
+            EncoderParameters encoderParams = new EncoderParameters(1);
+            encoderParams.Param[0] = qualityParam;
+
+            img.Save(path, jpegCodec, encoderParams);
+        }
+
+        /// <summary> 
+        /// Returns the image codec with the given mime type 
+        /// </summary> 
+        private static ImageCodecInfo GetEncoderInfo(string mimeType)
+        {
+            // Get image codecs for all image formats 
+            ImageCodecInfo[] codecs = ImageCodecInfo.GetImageEncoders();
+
+            // Find the correct image codec 
+            for (int i = 0; i < codecs.Length; i++)
+                if (codecs[i].MimeType == mimeType)
+                    return codecs[i];
+            return null;
+        } 
+        ///
+        /// Modes available for the resize method of images.
+        ///
+        public enum ResizeMode
+        {
+            ///
+            /// This will resize images to the resolution nearest to the target resolution. Images can become smaller when using this option
+            ///
+            Normal = 1,
+            ///
+            /// This will stretch an image so it always is the exact dimensions of the target resolution
+            ///
+            Stretch = 2,
+            ///
+            /// This will size an image to the exact dimensions of the target resolution, keeping ratio in mind and cropping parts that can't
+            /// fit in the picture.
+            ///
+            Crop = 3,
+            ///
+            /// This will size an image to the exact dimensions of the target resolution, keeping ratio in mind and filling up the image
+            /// with black bars when some parts remain empty.
+            ///
+            Fill = 4
+        }
+
+        ///
+        /// Method resizes the image so it fits the wanted resolution best.
+        ///
+        ///The image to be resized
+        ///The resolution which the image should be when the method is ready.
+        ///The mode for resizing the image.
+        public static System.Drawing.Image Resize(System.Drawing.Image image, Size targetResolution, ResizeMode resizeMode, System.Drawing.Color? background, bool rotate)
+        {
+            int sourceWidth = image.Width;
+            int sourceHeight = image.Height;
+            int targetWidth = targetResolution.Width;
+            int targetHeight = targetResolution.Height;
+            var fillBackground = background.HasValue? background.Value : Color.Transparent;
+            
+            // Supplied image is landscape, while the target resolution is portait OR
+            // supplied image is in portait, while the target resolution is in landscape.
+            // switch target resolution to match the image.
+            if ((sourceWidth > sourceHeight && targetWidth < targetHeight) || (sourceWidth < sourceHeight && targetWidth > targetHeight))
+            {
+                targetWidth = targetResolution.Height;
+                targetHeight = targetResolution.Width;
+            }
+            
+            float ratio = 0;
+            float ratioWidth = ((float)targetWidth / (float)sourceWidth);
+            float ratioHeight = ((float)targetHeight / (float)sourceHeight);
+            if (ratioHeight < ratioWidth)
+                ratio = ratioHeight;
+            else
+                ratio = ratioWidth;
+            Bitmap newImage = null;
+            switch (resizeMode)
+            {
+                case ResizeMode.Normal:
+                default:
+                    {
+                        int destWidth = (int)(sourceWidth * ratio);
+                        int destHeight = (int)(sourceHeight * ratio);
+                        newImage = new Bitmap(destWidth, destHeight);
+                        using (Graphics graphics = Graphics.FromImage((System.Drawing.Image)newImage))
+                        {
+                            graphics.Clear(fillBackground);
+                            graphics.InterpolationMode = InterpolationMode.HighQualityBicubic;
+                            graphics.DrawImage(image, 0, 0, destWidth, destHeight);
+                        }
+                        break;
+                    }
+                case ResizeMode.Crop:
+                    {
+                        if (ratioHeight > ratioWidth)
+                            ratio = ratioHeight;
+                        else
+                            ratio = ratioWidth;
+                        int destWidth = (int)(sourceWidth * ratio);
+                        int destHeight = (int)(sourceHeight * ratio);
+                        newImage = new Bitmap(targetWidth, targetHeight);
+                        int startX = 0;
+                        int startY = 0;
+                        if (destWidth > targetWidth)
+                            startX = 0 - ((destWidth - targetWidth) / 2);
+                        if (destHeight > targetHeight)
+                            startY = 0 - ((destHeight - targetHeight) / 2);
+                        using (Graphics graphics = Graphics.FromImage((System.Drawing.Image)newImage))
+                        {
+                            graphics.Clear(fillBackground);
+                            graphics.InterpolationMode = InterpolationMode.HighQualityBicubic;
+                            graphics.DrawImage(image, startX, startY, destWidth, destHeight);
+                        }
+                        break;
+                    }
+                case ResizeMode.Stretch:
+                    {
+                        newImage = new Bitmap(targetWidth, targetHeight);
+                        using (Graphics graphics = Graphics.FromImage((System.Drawing.Image)newImage))
+                        {
+                            graphics.Clear(fillBackground);
+                            graphics.InterpolationMode = InterpolationMode.HighQualityBicubic;
+                            graphics.DrawImage(image, 0, 0, targetWidth, targetHeight);
+                        }
+                        break;
+                    }
+                case ResizeMode.Fill:
+                    {
+                        newImage = new Bitmap(targetWidth, targetHeight);
+                        int destWidth = (int)(sourceWidth * ratio);
+                        int destHeight = (int)(sourceHeight * ratio);
+                        int startX = 0;
+                        int startY = 0;
+                        if (destWidth < targetWidth)
+                            startX = 0 + ((targetWidth - destWidth) / 2);
+                        if (destHeight < targetHeight)
+                            startY = 0 + ((targetHeight - destHeight) / 2);
+                        newImage = new Bitmap(targetWidth, targetHeight);
+                        using (Graphics graphics = Graphics.FromImage((System.Drawing.Image)newImage))
+                        {
+                            graphics.Clear(fillBackground);
+                            graphics.InterpolationMode = InterpolationMode.HighQualityBicubic;
+                            graphics.DrawImage(image, startX, startY, destWidth, destHeight);
+                        }
+                        break;
+                    }
+            }
+            return (System.Drawing.Image)newImage;
+        }
+    }
+}
---
+++ site/js/site.js
@@ -0,0 +1,208 @@
+/*
+* jQuery hashchange event - v1.3 - 7/21/2010
+* http://benalman.com/projects/jquery-hashchange-plugin/
+* 
+* Copyright (c) 2010 "Cowboy" Ben Alman
+* Dual licensed under the MIT and GPL licenses.
+* http://benalman.com/about/license/
+*/
+(function ($, e, b) { var c = "hashchange", h = document, f, g = $.event.special, i = h.documentMode, d = "on" + c in e && (i === b || i > 7); function a(j) { j = j || location.href; return "#" + j.replace(/^[^#]*#?(.*)$/, "$1") } $.fn[c] = function (j) { return j ? this.bind(c, j) : this.trigger(c) }; $.fn[c].delay = 50; g[c] = $.extend(g[c], { setup: function () { if (d) { return false } $(f.start) }, teardown: function () { if (d) { return false } $(f.stop) } }); f = (function () { var j = {}, p, m = a(), k = function (q) { return q }, l = k, o = k; j.start = function () { p || n() }; j.stop = function () { p && clearTimeout(p); p = b }; function n() { var r = a(), q = o(m); if (r !== m) { l(m = r, q); $(e).trigger(c) } else { if (q !== m) { location.href = location.href.replace(/#.*/, "") + q } } p = setTimeout(n, $.fn[c].delay) } $.browser.msie && !d && (function () { var q, r; j.start = function () { if (!q) { r = $.fn[c].src; r = r && r + a(); q = $('<iframe tabindex="-1" title="empty"/>').hide().one("load", function () { r || l(a()); n() }).attr("src", r || "javascript:0").insertAfter("body")[0].contentWindow; h.onpropertychange = function () { try { if (event.propertyName === "title") { q.document.title = h.title } } catch (s) { } } } }; j.stop = k; o = function () { return a(q.location.href) }; l = function (v, s) { var u = q.document, t = $.fn[c].domain; if (v !== s) { u.title = h.title; u.open(); t && u.write('<script>document.domain="' + t + '"<\/script>'); u.close(); q.location.hash = v } } })(); return j })() })(jQuery, this);
+
+
+$(function() {
+    //the loading image
+    var $loader = $('#st_loading');
+    //the ul element 
+    var $list = $('#st_nav');
+
+
+    //the current image being shown
+    var $currImage = $('#st_main').children('img:first');
+
+
+
+    var skipHashChange = false;
+    function imageChanged() {
+        if (!skipHashChange) {
+            var hash = window.location.hash.replace('!', '');
+            if (hash.length > 1) {
+                var tmp = $(hash);
+                if (tmp.length > 0) {
+                    tmp.click();
+                } else
+                    $list.find('img:first').click();
+            }
+
+            else
+                $list.find('img:first').click();
+        }
+    }
+
+    $(window).hashchange(imageChanged);
+
+    //let's load the current image 
+    //and just then display the navigation menu
+    $('<img>').load(function() {
+        $loader.hide();
+        $currImage.fadeIn(3000);
+        //slide out the menu
+        setTimeout(function() {
+            $list.animate({ 'left': '0px' }, 500);
+        },
+					1000);
+    }).attr('src', $currImage.attr('src'));
+
+    //calculates the width of the div element 
+    //where the thumbs are going to be displayed
+    buildThumbs();
+
+    function buildThumbs() {
+        $list.children('li.album').each(function() {
+            var $elem = $(this);
+            var $thumbs_wrapper = $elem.find('.st_thumbs_wrapper');
+            var $thumbs = $thumbs_wrapper.children(':first');
+            //each thumb has 180px and we add 3 of margin
+            var finalW = $thumbs.find('img').length * 183;
+            $thumbs.css('width', finalW + 'px');
+            //make this element scrollable
+            makeScrollable($thumbs_wrapper, $thumbs);
+        });
+    }
+
+
+    //clicking on the menu items (up and down arrow)
+    //makes the thumbs div appear, and hides the current 
+    //opened menu (if any)
+    $list.find('.st_arrow_down').live('click', function(e) {
+        var $this = $(this);
+        hideThumbs();
+        $this.addClass('st_arrow_up').removeClass('st_arrow_down');
+        var $elem = $this.closest('li');
+        $elem.addClass('current').animate({ 'height': '150px' }, 200);
+        var $thumbs_wrapper = $this.parent().next();
+        $thumbs_wrapper.show(200);
+        e.preventDefault();
+        return false;
+    });
+    $list.find('.st_arrow_up').live('click', function(e) {
+        var $this = $(this);
+        $this.addClass('st_arrow_down').removeClass('st_arrow_up');
+        hideThumbs();
+        e.preventDefault();
+        return false;
+    });
+
+    //clicking on a thumb, replaces the large image
+    $list.find('.st_thumbs img').bind('click', function(e) {
+
+        var $this = $(this);
+        skipHashChange = true;
+        window.location.hash = '!' + $this.attr('id');
+
+        $loader.show();
+        $('<img class="st_preview"/>').load(function() {
+            var $this = $(this);
+            var $currImage = $('#st_main').children('img:first');
+            $this.insertBefore($currImage);
+            $loader.hide();
+            
+            $currImage.remove();
+            
+            /*
+            $currImage.stop().fadeOut(2000, function () {
+            $(this).remove();
+            });
+            */
+            skipHashChange = false;
+        }).attr('src', $this.attr('href'));
+
+        $('h2').text($this.attr('title'));
+        attribUrl = $this.attr('data-attribution-url');
+        var atrib = $('#attribution').text($this.attr('data-attribution'));
+        if (attribUrl)
+            atrib.attr('href', attribUrl);
+        else
+            atrib.removeAttr('href');
+
+        e.preventDefault();
+        return false;
+    }).bind('mouseenter', function() {
+        $(this).stop().animate({ 'opacity': '1' });
+    }).bind('mouseleave', function() {
+        $(this).stop().animate({ 'opacity': '0.7' });
+    });
+
+    //function to hide the current opened menu
+    function hideThumbs(callback) {
+        var cb = callback;
+        $list.find('li.current')
+						 .animate({ 'height': '40px' }, 400, function() {
+						     $(this).removeClass('current');
+						     if (cb)
+						         cb();
+						 })
+						 .find('.st_thumbs_wrapper')
+						 .hide(200)
+						 .andSelf()
+						 .find('.st_link span')
+						 .addClass('st_arrow_down')
+						 .removeClass('st_arrow_up');
+    }
+
+    //makes the thumbs div scrollable
+    //on mouse move the div scrolls automatically
+    function makeScrollable($outer, $inner) {
+        var extra = 800;
+        //Get menu width
+        var divWidth = $outer.width();
+        //Remove scrollbars
+        $outer.css({
+            overflow: 'hidden'
+        });
+        //Find last image in container
+        var lastElem = $inner.find('img:last');
+        $outer.scrollLeft(0);
+        //When user move mouse over menu
+        $outer.unbind('mousemove').bind('mousemove', function(e) {
+            var containerWidth = lastElem[0].offsetLeft + lastElem.outerWidth() + 2 * extra;
+            var left = (e.pageX - $outer.offset().left) * (containerWidth - divWidth) / divWidth - extra;
+            $outer.scrollLeft(left);
+        });
+    }
+
+    var autoFirstHideTimer;
+
+    function hideAlbums() {
+        clearTimeout(autoFirstHideTimer);
+        $('.st_link').each(function() {
+            var st = $(this);
+            st.stop();
+
+            if (st.siblings(':visible').length == 0)
+                st.animate({ left: -(st.width() - 15) });
+        });
+    }
+
+    $('body').live('click', function() {
+        hideThumbs(hideAlbums);
+    });
+    function showAlbum(st) {
+        var st = $(st);
+        st.stop();
+        st.animate({ left: 0 }, function() {
+
+        });
+    }
+
+    $('.st_link').mouseout(function() {
+        hideAlbums();
+    });
+
+    $('.st_link').mouseover(function() {
+        showAlbum(this)
+    });
+
+    autoFirstHideTimer = setTimeout(hideAlbums, 5000);
+    imageChanged();
+
+});
